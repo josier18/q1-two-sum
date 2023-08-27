2fd74e1779c14365cdd7683ba7487406b01c1327
josier18 <joseline22729@gmail.com>
2023-08-26 22:42:21 -0500

    Added if statements and for loops.

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        
-        
-    }
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            for(int j = 1; j < nums.length - 1; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[]{i, j};
+                }          
+            }
+        }
 }
